6ddf566d514bcbbf2359c9f82c8de91612d878bb
Anderson Alves <anderson.filho.xap@gmail.com>
2026-08-27 14:46:47 -0500

    Adicionei Resposta!

--- basquete_robos.py
+++ basquete_robos.py
@@ -1,3 +1,10 @@
+from rich import print
 D_distancia = int(input('Digite a distancia: '))
 pontos = 0
-if 
+if D_distancia <= 800:
+    pontos = 1
+elif 800 < D_distancia <= 1400:
+    pontos = 2
+elif 1400 < D_distancia:
+    pontos = 3
+print(f'{D_distancia} -->  {pontos} Pontos')
